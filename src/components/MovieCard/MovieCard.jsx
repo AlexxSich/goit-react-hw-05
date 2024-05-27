bddf56f6d7aa42dc76721606e0bc19410adf922c
AlexxSich <147013014+AlexxSich@users.noreply.github.com>
2024-05-27 07:11:32 -0300

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -7,7 +7,7 @@ export default function MovieCard({ movie }) {
     'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg';
 
   return (
-    <Link to={`movies/${movie.id}`}>
+    <Link to={`/movies/${movie.id}`}>
       <div className={css.movieCardContainer}>
         <img
           src={
